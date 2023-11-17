b20a8e1827e820ee5ec81c408b6d9ef63a84d8a0
Carmen Caniglia <carmencaniglia@gmail.com>
2023-11-17 12:30:00 +0100

    icone preferiti fine

--- src/components/SearchJob.jsx
+++ src/components/SearchJob.jsx
@@ -1,28 +1,21 @@
 import { Card, Col, Row } from "react-bootstrap";
 import immagine from "../AvidCareerist.com-10.png";
-import { useDispatch } from "react-redux";
-import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const SearchJob = ({ jobsData }) => {
+  const favourites = useSelector((state) => state.jobs.favourites.content);
   const dispatch = useDispatch();
-  const [favourites, setFavourites] = useState({}); // Traccia i preferiti
 
   const toggleFavourite = (job) => {
-    const isFavourite = !!favourites[job._id];
+    const isFavourite = favourites.some((fav) => fav._id === job._id);
 
-    setFavourites({
-      ...favourites,
-      [job._id]: !isFavourite,
-    });
-
-    const actionType = isFavourite
-      ? "REMOVE_FROM_FAVOURITES"
-      : "ADD_TO_FAVOURITES";
-
-    dispatch({
-      type: actionType,
-      payload: job,
-    });
+    if (isFavourite) {
+      // Trova l'indice del lavoro nei preferiti
+      const index = favourites.findIndex((fav) => fav._id === job._id);
+      dispatch({ type: "REMOVE_FROM_FAVOURITES", payload: index });
+    } else {
+      dispatch({ type: "ADD_TO_FAVOURITES", payload: job });
+    }
   };
 
   return (
@@ -36,38 +29,48 @@ const SearchJob = ({ jobsData }) => {
         </div>
       )}
       {jobsData &&
-        jobsData.map((job) => (
-          <Card key={job._id}>
-            <Row className="container-fluid justify-content-center ">
-              <Col
-                md={2}
-                className="img-jobs-container rounded d-flex align-items-center ps-0 justify-content-center"
-              >
-                <Card.Img src={immagine} alt="job image" className="jobs-img" />
-              </Col>
-              <Col md={6} className="p-0">
-                <Card.Body className="lh-1">
-                  <Card.Link href={job.url} className="fw-bold" target="_blank">
-                    {job.title}
-                  </Card.Link>
-                  <Card.Title>{job.company_name}</Card.Title>
-                  <Card.Subtitle>{job.job_type}</Card.Subtitle>
-                </Card.Body>
-              </Col>
-              <Col md={2} className="ms-auto d-flex align-items-center fs-5">
-                <i className="bi bi-eye-slash-fill me-2"></i>
-                <i
-                  className={`bi ${
-                    favourites[job._id]
-                      ? "bi-bookmark-fill cursor-pointer"
-                      : "bi-bookmark cursor-pointer"
-                  }`}
-                  onClick={() => toggleFavourite(job)}
-                ></i>
-              </Col>
-            </Row>
-          </Card>
-        ))}
+        jobsData.map((job) => {
+          const isFav = favourites.some((fav) => fav._id === job._id);
+
+          return (
+            <Card key={job._id}>
+              <Row className="container-fluid justify-content-center">
+                <Col
+                  md={2}
+                  className="img-jobs-container rounded d-flex align-items-center ps-0 justify-content-center"
+                >
+                  <Card.Img
+                    src={immagine}
+                    alt="job image"
+                    className="jobs-img"
+                  />
+                </Col>
+                <Col md={6} className="p-0">
+                  <Card.Body className="lh-1">
+                    <Card.Link
+                      href={job.url}
+                      className="fw-bold"
+                      target="_blank"
+                    >
+                      {job.title}
+                    </Card.Link>
+                    <Card.Title>{job.company_name}</Card.Title>
+                    <Card.Subtitle>{job.job_type}</Card.Subtitle>
+                  </Card.Body>
+                </Col>
+                <Col md={2} className="ms-auto d-flex align-items-center fs-5">
+                  <i className="bi bi-eye-slash-fill me-2"></i>
+                  <i
+                    className={`bi ${
+                      isFav ? "bi-bookmark-fill" : "bi-bookmark"
+                    } cursor-pointer`}
+                    onClick={() => toggleFavourite(job)}
+                  ></i>
+                </Col>
+              </Row>
+            </Card>
+          );
+        })}
     </div>
   );
 };
